87fb5452fa142c6968d15314117c5a69952b65f4
Ron Kadar <qwazilla@gmail.com>
2017-12-20 18:51:40 -0800

    meh

--- content/build/all.js
+++ content/build/all.js
@@ -131,10 +131,22 @@
         var vm = this;
         vm.postIsh = postIsh;
         vm.$onInit = init;
+        vm.mapStyle;
+        vm.mapControl;
 
-        function init() {}
-
-        mapboxgl.accessToken = 'pk.eyJ1Ijoic25la2t5c25layIsImEiOiJjamF0eHd1Mzk1NjhvMnFvMmR3NGR2NDJtIn0._5awRs1gX159x4zFOz0V0g';
+        function init() {
+            mapboxgl.accessToken = 'pk.eyJ1Ijoic25la2t5c25layIsImEiOiJjamF0eHd1Mzk1NjhvMnFvMmR3NGR2NDJtIn0._5awRs1gX159x4zFOz0V0g';
+            vm.mapStyle = 'mapbox://styles/snekkysnek/cjbdax1157sf42slafagv5imd';
+            vm.mapControl = {
+                geocoder: {
+                    enabled: true,
+                    options: {
+                        position: 'top-left',
+                        accessToken: mapboxgl.accessToken
+                    }
+                }
+            };
+        }
 
         function postIsh(ting) {
             mappService.post(ting).then(function (data) {
